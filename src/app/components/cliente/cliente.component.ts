f90a5c27173671c86b67a6bb2790a03612882697
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2023-12-19 21:18:31 -0300

    Listagem Concluída

--- src/app/components/cliente/cliente.component.ts
+++ src/app/components/cliente/cliente.component.ts
@@ -1,13 +1,94 @@
-import {Component, ViewChild} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
 import {ClienteService} from "../../services/cliente.service";
-import {PoNotificationService} from "@po-ui/ng-components";
+import {PoDialogService, PoTableColumn} from "@po-ui/ng-components";
+import {Cliente} from "../../models/Cliente";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-cliente',
   templateUrl: './cliente.component.html',
   styleUrls: ['./cliente.component.scss']
 })
-export class ClienteComponent {
+export class ClienteComponent implements OnInit {
 
+  formCriarCliente: FormGroup = new FormGroup({})
+
+  clientes: Cliente[] = [];
+
+  colunas: Array<PoTableColumn> = [
+    {property: 'nome', label: 'Nome'},
+    {property: 'endereco', label: 'Endereço'},
+    {property: 'bairro', label: 'Bairro'},
+    {
+      property: 'telefones',
+      label: 'Telefones',
+      type: 'detail',
+      detail: {
+        columns: [
+          {property: 'telefone', label: 'Telefone'}
+        ]
+      }
+    },
+    {
+      property: 'acoes',
+      label: 'Ações',
+      type: 'icon',
+      sortable: false,
+      icons: [
+        {
+          action: this.editarCliente.bind(this),
+          icon: 'po-icon-edit',
+          tooltip: 'Editar Cliente',
+          value: 'editar'
+        }
+      ]
+    }
+  ]
+
+  constructor(private clienteService: ClienteService, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.criarFormInclusaoCliente();
+    this.listarClientes();
+  }
+
+  criarFormInclusaoCliente() {
+    this.formCriarCliente = this.formBuilder.group({
+      nome: ['', Validators.compose([Validators.required, Validators.minLength(11)])],
+      endereco: [''],
+      bairro: [''],
+      telefones: this.formBuilder.array([this.criarFormInclusaoTelefone('')])
+    });
+  }
+
+  private criarFormInclusaoTelefone(telefone: string): FormGroup {
+    return this.formBuilder.group({
+      telefone: [telefone]
+    });
+  }
+
+
+  listarClientes() {
+    this.clienteService.listar().subscribe((data: Cliente[]) => {
+      this.clientes = data;
+    });
+  }
+
+  editarCliente(cliente: Cliente) {
+    console.log(cliente);
+  }
+
+  salvarCliente() {
+
+  }
+
+  deleteTelefone(i: number) {
+
+  }
+
+  addtelefone() {
+    const phonesFormArray = this.formCriarCliente.get('telefones') as FormArray;
+    phonesFormArray.push(this.criarFormInclusaoTelefone(''));
+  }
 }
--- src/app/models/ClienteTelefone.ts
+++ src/app/models/ClienteTelefone.ts
@@ -1,4 +1,3 @@
 export class ClienteTelefone {
-  id ?: number;
   telefone ?: string
 }
